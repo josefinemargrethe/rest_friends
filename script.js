643ccf5560d655e46013ee6592f5205ae190d875
Josefine Margrethe Smidt Hansen <jose317f@stud.kea.dk>
2019-09-25 19:19:57 +0200

    added styled form to make new friend

--- script.js
+++ script.js
@@ -11,17 +11,26 @@ function get() {
   })
     .then(e => e.json())
     .then(friends => {
-      friends.forEach(getFriend);
+      console.log(friends);
+      friends.forEach(cleanJSON);
     });
 
-  document.querySelector("button").addEventListener("click", e => {
+  document.querySelector(".addBtn").addEventListener("click", e => {
     post();
   });
 }
 
-function getFriend(friend) {
+function cleanJSON(friend) {
+  const liPersonality = friend.personality.split(",");
+  console.log(liPersonality);
+
+  getFriend(friend, liPersonality);
+}
+
+function getFriend(friend, liPersonality) {
   const template = document.querySelector("template").content;
   const copy = template.cloneNode(true);
+  copy.querySelector("article.friend").dataset.friendid = friend._id;
   copy.querySelector("h1").textContent = friend.name;
   copy.querySelector("h3").textContent = `Also known as ${friend.nickname}`;
 
@@ -33,7 +42,14 @@ function getFriend(friend) {
   }
 
   copy.querySelector(".city").textContent = `${friend.nickname} lives in ${friend.city}`;
-  copy.querySelector(".personality").textContent = `${friend.name} to me is all of the following things: ${friend.personality}`;
+  //   copy.querySelector(".personality").textContent = `${friend.name} to me is all of the following things: ${friend.personality}`;
+  //personality[0] + "," personality[1] + "," + personality[2] + "," + personality[3];
+  copy.querySelector(".personality").innerHTML = `<article>${friend.name}'s best qualities:</article><article><ul><li>` + liPersonality + `</li></ul></article>`;
+
+  copy.querySelector(".deleteBtn").addEventListener("click", () => {
+    console.log(friend._id);
+    deleteFriend(friend._id);
+  });
   document.querySelector("#friends").appendChild(copy);
 }
 
@@ -61,7 +77,25 @@ function post() {
   })
     .then(res => res.json())
     .then(data => {
-      getFriend(data);
+      //getFriend(data);
+      cleanJSON(data);
       console.log(data);
+    });
+}
+
+function deleteFriend(id) {
+  fetch("https://boris4life-d49c.restdb.io/rest/friends/" + id, {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5d887c39fd86cb75861e2620",
+      "cache-control": "no-cache"
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      //TODO: Delete from DOM
+      console.log(`.friend[data-friendid="${id}"]`);
+      document.querySelector(`.friend[data-friendid="${id}"]`).remove();
     });
 }
